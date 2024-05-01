fb318fb12bc77326b16095999763576c634440ac
yo mama <tulgar.dinc@gmail.com>
2024-05-01 22:39:44 +0200

    stuff

---
+++ app/visual/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import FileView from "../components/FileView";
+import Title from "../components/Title";
+
+export default function Visual() {
+    return (
+        <div>
+            <Title>Visualisations</Title>
+            <FileView />
+        </div>
+    );
+}
