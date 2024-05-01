53fa4f2250d53a11662c93dc9ed152c8335a5627
Tulgar Dinç <tulgardinc@gmail.com>
2024-05-01 10:12:05 +0200

    logo

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,98 +2,97 @@
 
 import { useRouter } from "next/navigation";
 import { ChangeEvent, useState } from "react";
+import Image from "next/image";
 
 type NavLink = {
-    name: string;
-    route: string;
+  name: string;
+  route: string;
 };
 
 export default function NavBar() {
-    // pages
-    const users = { name: "Users", route: "/users" };
-    const tags = { name: "Tags", route: "/tags" };
-    const upload = { name: "Upload", route: "/upload" };
-    const train = { name: "Train Models", route: "/train" };
-    const trainResults = { name: "Training Results", route: "/trainresults" };
-    const run = { name: "Run Models", route: "/run" };
-    const runResults = { name: "Run Results", route: "/runresults" };
-    const file = { name: "File Explorer", route: "/file" };
+  // pages
+  const users = { name: "Users", route: "/users" };
+  const tags = { name: "Tags", route: "/tags" };
+  const upload = { name: "Upload", route: "/upload" };
+  const train = { name: "Train Models", route: "/train" };
+  const trainResults = { name: "Training Results", route: "/trainresults" };
+  const run = { name: "Run Models", route: "/run" };
+  const runResults = { name: "Run Results", route: "/runresults" };
+  const file = { name: "File Explorer", route: "/file" };
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const [userRole, setUserRole] = useState("admin");
-    const [navLinks, setNavLinks] = useState<NavLink[]>([users, tags, file]);
+  const [userRole, setUserRole] = useState("admin");
+  const [navLinks, setNavLinks] = useState<NavLink[]>([users, tags, file]);
 
-    const onNavClick = (route: string) => {
-        router.push(route);
-    };
+  const onNavClick = (route: string) => {
+    router.push(route);
+  };
 
-    const onRoleChange = (e: ChangeEvent<HTMLSelectElement>) => {
-        const val = e.target.value;
-        setUserRole(val);
-        switch (val) {
-            case "admin":
-                setNavLinks([users, tags, file]);
-                router.push("/users");
-                break;
-            case "provider":
-                setNavLinks([upload]);
-                router.push("/upload");
-                break;
-            case "engineer":
-                setNavLinks([upload]);
-                router.push("/upload");
-                break;
-            case "anotator":
-                setNavLinks([upload]);
-                router.push("/upload");
-                break;
-            case "researcher":
-                setNavLinks([train, trainResults]);
-                router.push("/train");
-                break;
-            case "scientist":
-                setNavLinks([upload, run, runResults]);
-                router.push("/run");
-                break;
-        }
-    };
+  const onRoleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const val = e.target.value;
+    setUserRole(val);
+    switch (val) {
+      case "admin":
+        setNavLinks([users, tags, file]);
+        router.push("/users");
+        break;
+      case "provider":
+        setNavLinks([upload]);
+        router.push("/upload");
+        break;
+      case "engineer":
+        setNavLinks([upload]);
+        router.push("/upload");
+        break;
+      case "anotator":
+        setNavLinks([upload]);
+        router.push("/upload");
+        break;
+      case "researcher":
+        setNavLinks([train, trainResults]);
+        router.push("/train");
+        break;
+      case "scientist":
+        setNavLinks([upload, run, runResults]);
+        router.push("/run");
+        break;
+    }
+  };
 
-    return (
-        <nav className="shadow-2xl h-screen flex flex-col justify-between">
-            <div className="flex justify-center items-center h-32">
-                <div className=" text-5xl font-bold">IMAGEN</div>
-            </div>
-            <div className="flex flex-col justify-between items-center h-28">
-                {navLinks &&
-                    navLinks.map((nav) => (
-                        <NavLink nav={nav} onClick={onNavClick} />
-                    ))}
-            </div>
-            <div className="flex justify-center items-center h-32">
-                <select onChange={onRoleChange} value={userRole}>
-                    <option value="admin">Admin</option>
-                    <option value="provider">Data Provider</option>
-                    <option value="engineer">Data Engineer</option>
-                    <option value="anotator">Data Anotator</option>
-                    <option value="researcher">AI Researcher</option>
-                    <option value="scientist">Behavioral Scientist</option>
-                </select>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="shadow-2xl h-screen flex flex-col justify-between">
+      <div className="flex justify-center items-center h-32">
+        <Image src={"/Logo.png"} alt={""} width={150} height={150} />
+      </div>
+      <div className="flex flex-col justify-between items-center h-28">
+        {navLinks &&
+          navLinks.map((nav) => <NavLink nav={nav} onClick={onNavClick} />)}
+      </div>
+      <div className="flex justify-center items-center h-32">
+        <select onChange={onRoleChange} value={userRole}>
+          <option value="admin">Admin</option>
+          <option value="provider">Data Provider</option>
+          <option value="engineer">Data Engineer</option>
+          <option value="anotator">Data Anotator</option>
+          <option value="researcher">AI Researcher</option>
+          <option value="scientist">Behavioral Scientist</option>
+        </select>
+      </div>
+    </nav>
+  );
 }
 
 function NavLink(props: {
-    nav: { name: string; route: string };
-    onClick: Function;
+  nav: { name: string; route: string };
+  onClick: Function;
 }) {
-    return (
-        <div
-            onClick={() => props.onClick(props.nav.route)}
-            className="text-gray-500 text-2xl font-semibold hover:cursor-pointer"
-        >
-            {props.nav.name}
-        </div>
-    );
+  return (
+    <div
+      onClick={() => props.onClick(props.nav.route)}
+      className="text-gray-500 text-2xl font-semibold hover:cursor-pointer"
+    >
+      {props.nav.name}
+    </div>
+  );
 }
